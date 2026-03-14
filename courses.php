bf8375d6ff63ce60e191c134b818c9295308a4b7
Mutiat Junaid <mutiatjunaid@gmail.com>
2026-03-14 14:35:56 +0000

    edit my courses page

--- courses.php
+++ courses.php
@@ -75,7 +75,7 @@
         <div class="container py-5">
             <div class="row justify-content-center">
                 <div class="col-lg-10 text-center">
-                    <h1 class="display-3 text-white animated slideInDown">Courses</h1>
+                    <h1 class="display-3 text-white animated slideInDown">I love my Family dearly</h1>
                     <nav aria-label="breadcrumb">
                         <ol class="breadcrumb justify-content-center">
                             <li class="breadcrumb-item"><a class="text-white" href="#">Home</a></li>
